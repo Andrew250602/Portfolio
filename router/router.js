98f9adc858a59c97a6cae7ceebf1a098aaaec9f2
Nguyễn Triêu Vỹ <2000000740@nttu.edu.vn>
2024-11-24 14:42:43 +0700

    Delete router directory

--- router/router.js
+++
@@ -1,9 +0,0 @@
-let homePage = require('../pages/home');
-let projectPage = require('../pages/project')
-
-const router = (app) => {
-    app.use(homePage)
-    app.use(projectPage)
-}
-
-module.exports = router
